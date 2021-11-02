e6b34dd773927a30b40ab3d29513d8f639368679
José Simão <jsimao@cc.isel.ipl.pt>
2021-11-02 18:28:23 +0000

    struts

---
+++ core/src/test/java/org/apache/struts2/views/jsp/ui/AnchorTest.java
@@ -0,0 +1,167 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.struts2.views.jsp.ui;
+
+import org.apache.struts2.TestAction;
+import org.apache.struts2.views.jsp.AbstractUITagTest;
+
+import javax.servlet.jsp.JspException;
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+
+
+/**
+ */
+public class AnchorTest extends AbstractUITagTest {
+
+    public void testBeanInfo() throws Exception {
+        BeanInfo beanInfo = Introspector.getBeanInfo(AbstractUITag.class);
+        for (PropertyDescriptor pd : beanInfo.getPropertyDescriptors()) {
+            System.out.println(pd.getName() + ": write = " + pd.getWriteMethod() + ", read = " + pd.getReadMethod());
+        }
+    }
+
+    public void testSimple() throws Exception {
+        createAction();
+
+        AnchorTag tag = createTag();
+        tag.setHref("a");
+        tag.doStartTag();
+        tag.doEndTag();
+
+        verifyResource("href-1.txt");
+    }
+
+    public void testSimpleBadQuote() throws Exception {
+        createAction();
+
+        AnchorTag tag = createTag();
+        tag.setHref("a\"");
+        tag.doStartTag();
+        tag.doEndTag();
+
+        verifyResource("href-2.txt");
+    }
+
+    public void testDynamicAttribute() throws Exception {
+        createAction();
+
+        AnchorTag tag = createTag();
+        tag.setHref("a");
+
+        tag.setDynamicAttribute("uri", "dynAttrName", "dynAttrValue");
+
+        tag.doStartTag();
+        tag.doEndTag();
+
+        verifyResource("Anchor-2.txt");
+    }
+
+    public void testDynamicAttributeAsExpression() throws Exception {
+        createAction();
+
+        AnchorTag tag = createTag();
+        tag.setHref("a");
+
+        tag.setDynamicAttribute("uri", "placeholder", "%{foo}");
+
+        tag.doStartTag();
+        tag.doEndTag();
+
+        verifyResource("Anchor-3.txt");
+    }
+
+    private void createAction() {
+        TestAction testAction = (TestAction) action;
+        testAction.setFoo("bar");
+    }
+
+    private AnchorTag createTag() throws JspException {
+        AnchorTag tag = new AnchorTag();
+        tag.setPageContext(pageContext);
+
+        tag.setId("mylink");
+        return tag;
+    }
+
+    /**
+     * Test anchor tag body supported
+     * 
+     * @throws Exception 
+     */
+    public void testSimpleWithBody() throws Exception {
+        createAction();
+
+        AnchorTag tag = createTag();
+        tag.setHref("a");
+
+        StrutsBodyContent body = new StrutsBodyContent(null);
+        body.print("normal body text with nothing to escape");
+        tag.setBodyContent(body);
+
+        tag.doStartTag();
+        tag.doEndTag();
+
+        verifyResource("href-3.txt");
+    }
+
+    /**
+     * Test that by default anchor tag body is HTML-escaped.
+     * 
+     * @throws Exception 
+     */
+    public void testSimpleWithBodyHTMLEscaped() throws Exception {
+        createAction();
+
+        AnchorTag tag = createTag();
+        tag.setHref("a");
+
+        StrutsBodyContent body = new StrutsBodyContent(null);
+        body.print("should HTML escape: < & >");
+        tag.setBodyContent(body);
+
+        tag.doStartTag();
+        tag.doEndTag();
+
+        verifyResource("href-4.txt");
+    }
+
+    /**
+     * Test that with htmlEscapeBody false anchor tag body is not HTML-escaped.
+     * 
+     * @throws Exception 
+     */
+    public void testSimpleWithBodyNotHTMLEscaped() throws Exception {
+        createAction();
+
+        AnchorTag tag = createTag();
+        tag.setHref("a");
+        tag.setEscapeHtmlBody(false);
+
+        StrutsBodyContent body = new StrutsBodyContent(null);
+        body.print("should not HTML escape: < & >");
+        tag.setBodyContent(body);
+
+        tag.doStartTag();
+        tag.doEndTag();
+
+        verifyResource("href-5.txt");
+    }
+}
